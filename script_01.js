55f092431d1136d620dc395b6771920d6eefe083
Peter <schmidt.multimedia@gmail.com>
2021-05-19 09:30:16 +0200

    control structures | Vorbereitung

--- script_01.js
+++ script_01.js
@@ -1,1 +1,22 @@
 // Entscheidungsstrukturen | control structures
+
+// Deklaration
+let ageJohn, ageMark;
+let isJohnOlder, isJohnEqual;
+
+// Wertzuweisung
+ageJohn = 30;
+ageMark = 30;
+
+//Logische Aussagen/ Test(s)
+isJohnOlder = (ageJohn > ageMark);
+isJohnEqual = (ageJohn == ageMark);
+
+// Ausgabe
+console.log("isJohnOlder: " + isJohnOlder);
+console.log("isJohnEqual: " + isJohnEqual);
+
+/************ IF  ************/
+// alternativlos (TINA)
+
+
